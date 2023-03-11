501019c25e8e7b75541f53d90dc811ffb6313520
johnyboygit <johny22224444@gmail.com>
2023-03-11 14:19:03 +0100

    category management added

---
+++ EcommerceBooksWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EcommerceBooksWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EcommerceBooksWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
